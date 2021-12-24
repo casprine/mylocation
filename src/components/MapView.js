7a6443edacb2457542071b6b3e22d119f4525290
casprine <casprine.001@gmail.com>
2021-12-24 16:33:04 +0000

    chore: fix build

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -14,7 +14,7 @@ export const MapView = ({ position }) => {
 
   return (
     <div className="map-container">
-      <Wrapper apiKey={procces.env.REACT_APP_API_KEY} render={render}>
+      <Wrapper apiKey={process.env.REACT_APP_API_KEY} render={render}>
         <Map center={position} zoom={zoom} style={{ flexGrow: '1', height: '100%' }}>
           <Marker position={position} />
         </Map>
